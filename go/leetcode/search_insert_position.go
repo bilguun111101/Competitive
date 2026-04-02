b48ae52bbc47afe2d04e107592326abd9e863b88
bilguun111101 <102782221+bilguun111101@users.noreply.github.com>
2026-04-02 16:31:45 +0800

    fix/search_insert_position min

--- go/leetcode/search_insert_position.go
+++ go/leetcode/search_insert_position.go
@@ -1,15 +1,26 @@
 package leetcode
 
-// nums := []int16{1, 3, 5, 7, 9, 11}
-// target := 6
-// target := 4
+func min(a, b int) int {
+	if a < b {
+		return a
+	}
+	return b
+}
+func max(a, b int) int {
+	if a > b {
+		return a
+	}
+	return b
+}
+
+// nums := []int16{1, 3, 5, 6}
+// 10
+
 func SearchInsert(nums []int16, target int16) int {
 	left, right := 0, len(nums)
 
-	var mid int
-
 	for i := 0; i < len(nums); i++ {
-		mid = left + (right-left)/2
+		mid := min(left+(right-left)/2, len(nums)-1)
 		if nums[mid] < target {
 			left = mid + 1
 		} else {
--- go/main.go
+++ go/main.go
@@ -7,7 +7,9 @@ import (
 
 func main() {
 	nums := []int16{1, 3, 5, 7, 9, 11}
+	// nums := []int16{1, 3, 5, 6}
 
-	result_index := leetcode.SearchInsert(nums, 6)
+	result_index := leetcode.SearchInsert(nums, 10)
+	// result_index := leetcode.SearchInsert(nums, 7)
 	fmt.Println("index: ", result_index)
 }
